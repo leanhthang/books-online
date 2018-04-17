4c3c9722a8fb5dfbc57cb25459f2ad1275e4f9ef
Thang <leanhthang70@gmail.com>
2018-04-18 03:47:58 +0700

    update client navbar and search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.paginate(:page => params[:page], :per_page => 15)
+    if params[:key_word]
+      @posts = search(params[:key_word])
+    else
+      @posts = Post.includes(:author)
+                   .paginate(:page => params[:page], :per_page => 20)
+   end
   end
 end
